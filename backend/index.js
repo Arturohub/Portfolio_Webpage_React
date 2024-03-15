054218d993e519eb7f0976ab5282bf33740fbd52
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-03-15 13:17:23 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,39 +1,29 @@
-const express = require("express")
-const cors = require("cors")
-const router = require("./routes/router")
-const mongoose = require("mongoose")
-require("dotenv/config")
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes/router");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const corsOptions = {
-    origin: ["https://arturochicavilla-backend.onrender.com", "https://arturochicavilla.onrender.com"],
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+  origin: ["https://arturochicavilla-backend.onrender.com", "https://arturochicavilla.onrender.com"],
+  credentials: true,
+};
 
-app.options('/', function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader('Access-Control-Allow-Methods', '*');
-    res.setHeader("Access-Control-Allow-Headers", "*");
-    res.end();
-  });
+app.use(cors(corsOptions));
+app.use("/", router);
 
-app.use("/", router)
-
-const dbOptions = {useNewUrlParser:true, useUnifiedTopology:true}
+const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 mongoose.connect(process.env.DB_URI, dbOptions)
-.then(() => console.log("DB Connected!"))
-.catch((error => console.log(error)))
+  .then(() => console.log("DB Connected!"))
+  .catch(error => console.log(error));
 
-const port = process.env.PORT
+const port = process.env.PORT;
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
-
-
+  console.log(`Server is running on port ${port}`);
+});
