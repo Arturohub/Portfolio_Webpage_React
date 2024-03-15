d3bcd5894cc41366da4d679b5e3022cc2722d44c
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-03-15 11:02:44 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,12 +9,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-const corsOptions = {
-    origin: "*",
-    credentials: true,
-    optionSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+app.use(cors())
 
 app.use("/", router)
 
