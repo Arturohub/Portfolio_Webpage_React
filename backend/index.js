46f41a2302ff7347e6566a232e1864a30d602285
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-03-15 12:17:18 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 const corsOptions = {
-    origin: "https://arturochicavilla.onrender.com/",
+    origin: "https://arturochicavilla-backend.onrender.com/",
     credentials: true,
     optionSuccessStatus: 200
 }
