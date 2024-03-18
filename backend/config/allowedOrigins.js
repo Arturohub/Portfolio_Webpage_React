2bb0aa10f4775416cab86a7d3048d68e294e8d1c
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-03-18 11:19:37 +0100

    Update allowedOrigins.js

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
     "http://localhost:3000",
     "https://portfolio-webpage-react-frontend.vercel.app",
     "https://portfolio-webpage-react-backend.vercel.app",
-    "https://portfolio-webpage-react-frontend/contact.vercel.app"
+    "https://portfolio-webpage-react-frontend.vercel.app/contact",
+    "https://portfolio-webpage-react-backend.vercel.app/contact"
 ]
 
 module.exports = allowedOrigins
