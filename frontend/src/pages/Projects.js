5518727ae28cf73710828cf022da35f40f5eca20
Arturo Chica Villa <arturochicavilla@hotmail.com>
2024-05-03 18:24:47 +0200

    Update Projects.js

--- frontend/src/pages/Projects.js
+++ frontend/src/pages/Projects.js
@@ -6,8 +6,11 @@ export default function Projects() {
     const projects = [
         {url: "https://arturohub.github.io/Furniture-Restoration-Website/", title: "Project 1. Furniture Restoration Shop"},
         {url: "https://arturohub.github.io/Todo-App-React/", title: "Project 2. To-Do App"},
-        {url: "https://arturohub.github.io/FoodApp/", title: "Project 3. Food Recipe App"}
-    ]
+        {url: "https://arturohub.github.io/FoodApp/", title: "Project 3. Food Recipe App"},
+        {url: "https://proyectoescrache.onrender.com", title: "Project 4. Heavy Band Webpage"},
+        {url: "https://restorationshop.onrender.com", title: "Project 5. Revamped Restoration Shop"},
+    
+        ]
 
     const [page, setPage] = useState(0)
     
